06316ff91a8f324a83be554afdeac41a0e5ae334
Varun <138992240+devvarun001@users.noreply.github.com>
2023-07-11 12:03:02 +0530

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int age ;
+	printf("Created by Dev.Varun \n");
+	printf("you must be 18 for account \n");
+	printf("Enter you age = \n");
+	scanf("%i", &age);
+	if(age>=18)
+	printf("Your are able to create Account!");
+	else
+	printf("Sorry You are not Able To Create account \n");
+	printf("Coded By Dev.varun \n");	
+}
